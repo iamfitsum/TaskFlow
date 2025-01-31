3d1077a3f11b47f502f7211a3be78eaaab197fe2
iamfitsum <official.fitsum.mekonnen@gmail.com>
2025-02-01 00:12:30 +0300

    Added Sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -19,15 +19,18 @@ const Sidebar = () => {
         </div>
         {/* Team */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:bg-gray-700">
-        <Image src="/logo.png" alt="Logo" width={40} height={40}  />
-        <div>
-          <h3 className="text-[14px] font-bold tracking-wide dark:text-gray-200">TaskFlow Team</h3>
-          <div className="mt-1 flex items-start gap-2">
-          <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
-          <p className="text-xs text-gray-500">Private</p>
+          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+          <div>
+            <h3 className="text-[15px] font-bold dark:text-gray-200">
+              TaskFlow Team
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
           </div>
         </div>
-        </div>
+        {/* Navbar Links */}
       </div>
     </div>
   );
