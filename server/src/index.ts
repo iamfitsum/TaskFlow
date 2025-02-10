3a4433168eb9a41fb677e786f4ef5c4cfa1482c6
iamfitsum <official.fitsum.mekonnen@gmail.com>
2025-02-11 02:53:33 +0300

    Setup server

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,11 +7,27 @@ import morgan from "morgan";
 // TODO: Route imports
 
 // Configurations
+dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(express.json())
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({
+  policy: "cross-origin"
+}))
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ 
+  extended: false 
+}));
 app.use(cors());
+
+// Routes
+app.get("/", (req, res) => { 
+  res.send("This is home route");
+})
+
+// Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
