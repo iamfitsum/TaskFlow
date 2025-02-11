25867a9fb97e7a05cf7d1280f81e43a74b236c8d
iamfitsum <official.fitsum.mekonnen@gmail.com>
2025-02-11 23:55:00 +0300

    Added project backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-// TODO: Route imports
+// Route imports
+import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
+import searchRoutes from "./routes/searchRoutes";
+import userRoutes from "./routes/userRoutes";
+import teamRoutes from "./routes/teamRoutes";
 
 // Configurations
 dotenv.config();
@@ -25,6 +30,12 @@ app.use(cors());
 app.get("/", (req, res) => { 
   res.send("This is home route");
 })
+
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
+app.use("/teams", teamRoutes);
 
 // Server
 const port = process.env.PORT || 3000;
