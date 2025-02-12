6f86ef884dbf3f2a8c11b6f7b855cff16a206260
iamfitsum <official.fitsum.mekonnen@gmail.com>
2025-02-12 23:26:22 +0300

    Code Cleanup

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -9,7 +9,12 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
-// TODO: Route imports
+// Route imports
+const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
+const taskRoutes_1 = __importDefault(require("./routes/taskRoutes"));
+const searchRoutes_1 = __importDefault(require("./routes/searchRoutes"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const teamRoutes_1 = __importDefault(require("./routes/teamRoutes"));
 // Configurations
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -28,6 +33,11 @@ app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("This is home route");
 });
+app.use("/projects", projectRoutes_1.default);
+app.use("/tasks", taskRoutes_1.default);
+app.use("/search", searchRoutes_1.default);
+app.use("/users", userRoutes_1.default);
+app.use("/teams", teamRoutes_1.default);
 // Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
